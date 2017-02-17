2d2983fad04c24254cd910b4bdbdbcbafe4abc01
AdrianAgramon <a.agramon@alumnos.upm.es>
2017-02-17 09:55:27 +0100

    Cambios en el mapa, ya se puede hacer un marcador. Queda lo de la herencia de datos entre activities.

--- app/src/main/java/com/example/adrin/location/MapsActivity.java
+++ app/src/main/java/com/example/adrin/location/MapsActivity.java
@@ -1,11 +1,12 @@
 package com.example.adrin.location;
 
-import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.icu.text.DecimalFormat;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,33 +18,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,  GoogleMap.OnMapClickListener {
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    private GoogleMap mapa;
 
-    private GoogleMap mMap;
-
-    Intent intent = getIntent();
-    Bundle b = intent.getExtras();
-    double[] coord = (double[]) b.getSerializable("coordenadas");
+    private final LatLng objetivo = new LatLng(39.481106, -0.340987);
 
 
-
-
-    private final LatLng obj = new LatLng( coord[0],coord[1]);
-
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapa);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapa);
         mapFragment.getMapAsync(this);
 
         final Button botonDist = (Button) findViewById(R.id.resolver);
@@ -54,51 +43,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onClick(View view) {
 
-                dist.setText(" "+(Double) CalculationByDistance(obj,Marker2));
+                dist.setText(" "+(int) CalculationByDistance(objetivo,Marker2));
 
             }
         });
-    //Try
     }
 
+    @Override public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(objetivo, 4));
+        mapa.setOnMapClickListener(this);
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mapa.setMyLocationEnabled(true);
+            mapa.getUiSettings().setZoomControlsEnabled(false);
+            mapa.getUiSettings().setCompassEnabled(true);
+        }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng europe = new LatLng(47.57652571374621, 7.55859375);
+    public void moveCamera(View view) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(objetivo));
+    }
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(europe));
+    public void animateCamera(View view) {
+        if (mapa.getMyLocation() != null)
+            mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mapa.getMyLocation().getLatitude(),
+                            mapa.getMyLocation().getLongitude()), 15));
     }
 
     public void addMarker(View view) {
-        mMap.addMarker(new MarkerOptions().position(
-                mMap.getCameraPosition().target));
+        mapa.addMarker(new MarkerOptions().position(
+                mapa.getCameraPosition().target));
     }
 
     LatLng Marker2 =new LatLng (0,0);
-
+    int i =1;
     @Override public void onMapClick(LatLng puntoPulsado) {
 
-        mMap.clear();
 
-        mMap.addMarker(new MarkerOptions().position(puntoPulsado)
+        mapa.clear();
+
+        Marker mark = mapa.addMarker(new MarkerOptions().position(puntoPulsado)
                 .icon(BitmapDescriptorFactory
                         .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
 
+
         final LatLng Marcador1 = new LatLng( puntoPulsado.latitude , puntoPulsado.longitude);
         Marker2=Marcador1;
+
+
+
+
 
     }
 
